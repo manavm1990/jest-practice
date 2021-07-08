c1cbe45f53308e48763ad61c8f939d945af3e80e
Manav Misra <manavm@visionwebsoft.com>
2021-07-08 17:07:22 -0500

    :poop: WIP Implement solutions

--- app/index.test.js
+++ app/index.test.js
@@ -1,9 +1,44 @@
-// TODO: `import` all other fxns. from `index.js`
-// Write the tests ✅.
-// DON'T COPY/PASTE - time on task 👩🏾‍💻 ⌨️.
-import { add } from "./index.js";
+import axios from "axios";
+import { add, getUsers } from "./index.js";
 
-// TODO: Add `describe` as appropriate to group the similar types of tests.
 it("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
+});
+
+// In order to test this w/o making an actual request, we can use a mock.
+jest.mock("axios");
+
+describe("getUsers", () => {
+  it("gets users", () => {
+    const mockResponse = {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
+    };
+
+    axios.mockResolvedValue(mockResponse);
+
+    // `toEqual` uses strict equality. Use it for objects (non-primitives).
+    return getUsers.then((response) =>
+      expect(response[0]).toEqual(mockResponse)
+    );
+  });
 });
