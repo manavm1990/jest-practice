69e2a61671c9a10343158945201eb0cf1f99d69a
Manav Misra <manavm@visionwebsoft.com>
2021-07-08 16:39:43 -0500

    :bulb: Add TODOS for test file

--- app/index.test.js
+++ app/index.test.js
@@ -1,5 +1,9 @@
-import add from "./index.js";
+// TODO: `import` all other fxns. from `index.js`
+// Write the tests ✅.
+// DON'T COPY/PASTE - time on task 👩🏾‍💻 ⌨️.
+import { add } from "./index.js";
 
+// TODO: Add `describe` as appropriate to group the similar types of tests.
 it("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
